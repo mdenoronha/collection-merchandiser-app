9bfb2d8c5b5d472a40ccd3e74394bcd8bde3a8b5
Matthew de Noronha <matthewdenoronha1@Matthews-MacBook-Pro.local>
2019-08-30 23:36:18 +0100

    Make debug false

--- app.py
+++ app.py
@@ -314,5 +314,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")))
 

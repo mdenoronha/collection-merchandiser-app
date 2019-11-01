9948cee1e4685d469e92cfd517cbf5fb818838bb
Matthew <matthewdenoronha@gmail.com>
2019-11-01 19:10:30 +0000

    Specify redis url

--- worker.py
+++ worker.py
@@ -6,7 +6,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low'] # while scheduling the task in views.py we sent it to default
 
-redis_url = 'redis://redistogo:b8b8ce0e7548ee29c447d4d8eb84c63a@porgy.redistogo.com:11667/'
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 # redis://redistogo:b8b8ce0e7548ee29c447d4d8eb84c63a@porgy.redistogo.com:11667/
 
 conn = redis.from_url(redis_url)

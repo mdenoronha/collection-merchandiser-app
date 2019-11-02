89d0e412d90ce6e89ae7e049e99053122edcb860
Matthew <matthewdenoronha@gmail.com>
2019-11-02 12:37:07 +0000

    Add print URL check

--- worker.py
+++ worker.py
@@ -7,16 +7,10 @@ from rq import Worker, Queue, Connection
 listen = ['high', 'default', 'low'] # while scheduling the task in views.py we sent it to default
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-# redis://redistogo:b8b8ce0e7548ee29c447d4d8eb84c63a@porgy.redistogo.com:11667/
+print("Redis URL", redis_url)
 
 conn = redis.from_url(redis_url)
 
-
-# redis_url = os.getenv('REDISTOGO_URL')
-
-# urlparse.uses_netloc.append('redis')
-# url = urlparse.urlparse(redis_url)
-# conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
     with Connection(conn):

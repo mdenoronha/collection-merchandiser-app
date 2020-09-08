b5becb8e62717da6e4805ed1c1cdb52ffd2a273e
Matthew <matthewdenoronha@gmail.com>
2020-09-08 22:49:32 +0100

    Add new collection page

---
+++ graphql.py
@@ -0,0 +1,153 @@
+import requests
+from python_graphql_client import GraphqlClient
+
+def reorderProducts(store, access_token, collection_id, changes):
+
+	headers = {
+        "X-Shopify-Access-Token": access_token,
+        "Content-Type": "application/json"
+    }
+
+	client = GraphqlClient(endpoint="https://" + store + "/admin/api/2020-04/graphql.json")
+	
+	query = """
+	mutation collectionReorderProducts($id: ID!, $moves: [MoveInput!]!) {
+	  collectionReorderProducts(id: $id, moves: $moves) {
+	    job {
+	      id
+	    }
+	    userErrors {
+	      field
+	      message
+	    }
+	  }
+	}
+	"""
+
+	if not 'gid://shopify/Collection/' in collection_id:
+		collection_id = 'gid://shopify/Collection/{0}'.format(collection_id)
+	variables = {'id': collection_id, 'moves': changes};
+
+	data = client.execute(query=query, headers=headers, variables=variables)
+
+	return data
+
+# Used for manual sort page
+def queryProducts(store, access_token, collection_id, cursor=None):
+
+	headers = {
+        "X-Shopify-Access-Token": access_token,
+        "Content-Type": "application/json"
+    }
+
+	client = GraphqlClient(endpoint="https://" + store + "/admin/api/2020-04/graphql.json")
+	
+	query = """
+    query MyQuery($collection: ID!, $cursor: String) {
+	  collection(id: $collection) {
+	  	productsCount
+	  	sortOrder
+	    products(first: 250, after: $cursor) {
+	      pageInfo {
+	        hasNextPage
+	        hasPreviousPage
+	      }
+	      edges {
+	        cursor
+	        node {
+	          title
+	          onlineStoreUrl
+	          id
+	          createdAt
+	          priceRange {
+	            minVariantPrice {
+	              amount
+	              currencyCode
+	            }
+	          }
+	          tracksInventory
+	          totalInventory
+	          featuredImage {
+                transformedSrc(maxHeight: 300)
+              }
+	        }
+	      }
+	    }
+	  }
+	}
+	"""
+	
+
+	variables = {'collection': 'gid://shopify/Collection/{0}'.format(collection_id), 'cursor': cursor};
+
+	data = client.execute(query=query, headers=headers, variables=variables)
+
+	return data
+
+def queryCollectionsOfProducts(store, access_token, product_id, cursor=None):
+
+	headers = {
+        "X-Shopify-Access-Token": access_token,
+        "Content-Type": "application/json"
+    }
+
+	client = GraphqlClient(endpoint="https://" + store + "/admin/api/2020-04/graphql.json")
+	
+	query = """
+    query MyQuery($product: ID!, $cursor: String) {
+      product(id: $product) {
+        title
+        collections(first: 250, after: $cursor) {
+        	pageInfo {
+        	  hasNextPage
+        	  hasPreviousPage
+        	}
+	        edges {
+	        	cursor
+	          node {
+	            id
+	            sortOrder
+	            productsCount
+	          }
+	      	}
+	    	}
+      }
+	}
+	"""
+
+	variables = {'product': 'gid://shopify/Product/{0}'.format(product_id), 'cursor': cursor};
+
+	data = client.execute(query=query, headers=headers, variables=variables)
+
+	return data
+
+def collectionCreate(store, access_token):
+
+	headers = {
+        "X-Shopify-Access-Token": access_token,
+        "Content-Type": "application/json"
+    }
+
+	client = GraphqlClient(endpoint="https://" + store + "/admin/api/2020-04/graphql.json")
+	
+	query = """
+  mutation collectionCreate($input: CollectionInput!) {
+    collectionCreate(input: $input) {
+      collection {
+        id
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+	"""
+
+	variables = {'input': 'gid://shopify/Product/{0}'.format(product_id), 'cursor': cursor};
+
+	data = client.execute(query=query, headers=headers, variables=variables)
+
+	return data
+
+
